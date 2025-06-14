7074272f09d650569f4ede209ea5ecc7f6b68122
R.Keelan <R.Keelan@gmail.com>
2025-06-14 14:08:05 -0400

    Initial project structure

---
+++ CompactFrameFormat.Tests/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CompactFrameFormat.Tests;
+
+public class CompactFrameFormatTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        // Setup code will go here when we implement the actual CFF functionality
+    }
+
+    [Test]
+    public void PlaceholderTest_ShouldPass_UntilRealImplementation()
+    {
+        // This is a placeholder test that will be replaced with real tests
+        // when the CompactFrameFormat library is implemented
+        Assert.Pass("Placeholder test - implementation pending");
+    }
+
+    [Test]
+    public void CanInstantiateClass1_ShouldNotThrow()
+    {
+        // Basic test to ensure the library can be referenced and classes instantiated
+        var instance = new Class1();
+        Assert.That(instance, Is.Not.Null);
+    }
+}
---
+++ CompactFrameFormat/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CompactFrameFormat
+{
+    public class Class1
+    {
+
+    }
+} 
